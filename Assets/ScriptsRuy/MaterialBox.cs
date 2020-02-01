e9d9e5658361a31967c393d7f10c3ea0dd3c956d
Ruy "Lorde Supremo " Oliveira <ruygsgoliveira@live.com>
2020-02-01 16:36:20 -0300

    craft process

---
+++ Assets/ScriptsRuy/MaterialBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialBox : MonoBehaviour
+{
+    public GameObject materialPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject obj = Instantiate(materialPrefab, col.transform.position, col.transform.rotation);
+            col.gameObject.GetComponent<Player>().PutInContainer(obj);
+            Debug.Log(this.transform.name);
+        }
+        
+    }
+}
---
+++ Assets/ScriptsRuy/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject container;
+    public LayerMask collisionLayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PutInContainer(GameObject obj)
+    {
+        this.container = obj;
+        obj.transform.parent = this.transform;
+    }
+
+    public GameObject Give()
+    {
+        return this.container;
+    }
+    
+    void OnTriggerStay(Collider col)
+    {
+        //Debug.Log(col.transform.name + " " + col.transform.tag);
+    }
+}
