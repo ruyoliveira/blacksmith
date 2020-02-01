206ffaf8b52bb4f20d2299e53456cf79d7dbb7ce
Tiago2R <tiago.rr@outlook.com>
2020-02-01 17:13:54 -0500

    Basic order creation and completion

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    public GameObject jobs;
+    private Order orders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orders = jobs.GetComponent<Order>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Player player = col.gameObject.GetComponent<Player>();
+            GameObject givenProduct = player.Give();
+            if (givenProduct != null)
+            {
+                if (orders.CompleteOrder(givenProduct))
+                {
+                    Destroy(givenProduct);
+                }
+            }
+            Debug.Log(this.transform.name);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour
+{
+    public GameObject orderPrefab;
+    public float newOrderInterval = 15.0f;
+
+    public List<int> orders; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orders = new List<int>();
+        InvokeRepeating("CreateOrder", 1.0f, newOrderInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // New orders
+    void CreateOrder()
+    {
+        orders.Add(100);
+        GameObject obj = Instantiate(orderPrefab, transform.position, transform.rotation, transform);
+    }
+
+    // Delivered Order
+    public bool CompleteOrder(GameObject product)
+    {
+        int productId = product.GetComponent<Id>().id;
+        // Check if there's a order pending for this product
+        if (orders.Contains(productId)) 
+        {
+            RemoveOrder(orders.LastIndexOf(productId));
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void RemoveOrder(int orderIndex)
+    {
+        int completedOrderIndex = orders.LastIndexOf(orderIndex);
+        orders.RemoveAt(orderIndex);
+        Destroy(transform.GetChild(orderIndex).gameObject);
+    }
+}
