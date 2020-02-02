6c6dce3f0e6fcb6108595dc26ace1678c52119d4
Tiago2R <tiago.rr@outlook.com>
2020-02-02 14:04:00 -0500

    Added multiplayer

---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    private int maxPlayers = 2;
+    private bool[] instatiatedPlayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Input.GetJoystickNames());
+        instatiatedPlayers = new bool[maxPlayers];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            if (Input.GetButtonDown("Fire" + i) && !instatiatedPlayers[i])
+            {
+                GameObject player = Instantiate(playerPrefab, transform.position, transform.rotation, transform);
+                player.GetComponent<DebugMove>().input = i;
+                player.transform.Find("PlayerActionCollider").GetComponent<Player>().id = i;
+                instatiatedPlayers[i] = true;
+            }
+        }
+    }
+}
